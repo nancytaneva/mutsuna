93557346a2ee0bd3381bc39987d900ed34a97c4f
Nancy Taneva <nancy.taneva@gmail.com>
2023-07-29 12:43:54 +0300

    Created Pet age Calculator

--- calculator.js
+++ calculator.js
@@ -1,24 +1,152 @@
-const calendarAgeYears = document.getElementById("calendar-age-years").value;
-console.log(`${calendarAgeYears}`);
+const calculateAgeBtn = document.getElementById("calculateAgeBtn");
 
-const calendarAgeMonths = document.getElementById("calendar-age-months").value;
-console.log(`${calendarAgeMonths}`);
+calculateAgeBtn.addEventListener('click', function () {
+    
+    // console.log('calculateAgeBtn:', calculateAgeBtn);
 
-function calculateAgeFunction(calendarAgeYears, calendarAgeMonths) {
-    // var realAge = 0;
-    // var realWord = "";
-    if(calendarAgeYears < 1 && calendarAgeMonths <= 1) {
-    console.log(`1 година`);
-    }
-    if(calendarAgeYears < 1 && calendarAgeMonths == 2) {
-        console.log(`3 години`);
+    const calendarAgeYears = document.getElementById("calendar-age-years").value  || 0;
+    // console.log(`${calendarAgeYears}`);
+
+    const calendarAgeMonths = document.getElementById("calendar-age-months").value || 0;
+    // console.log(`${calendarAgeMonths}`);
+
+    var humanAgeYears = 0;
+   
+    var yearsInMonths = calendarAgeYears * 12;
+    var ageInMonths = parseInt(yearsInMonths) + parseInt(calendarAgeMonths);
+    console.log('ageInMonths:',  ageInMonths);
+
+
+    if(ageInMonths <= 24) {
+        if(ageInMonths == 1) {
+            humanAgeYears = humanAgeYears + 1;
+            console.log(`humanAgeYears:`, humanAgeYears);
         }
-}
+
+        if(ageInMonths == 2) {
+            humanAgeYears = humanAgeYears + 3;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+
+        if(ageInMonths == 3) {
+            humanAgeYears = humanAgeYears + 4;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+
+        if(ageInMonths == 4) {
+            humanAgeYears = humanAgeYears + 6;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+
+        if(ageInMonths == 5) {
+            humanAgeYears = humanAgeYears + 8;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+
+        if(ageInMonths == 6) {
+            humanAgeYears = humanAgeYears + 10;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+
+        if(ageInMonths == 7) {
+            humanAgeYears = humanAgeYears + 12;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+
+        if(ageInMonths > 7 && ageInMonths < 12) {
+            if(ageInMonths == 8) {
+                humanAgeYears = humanAgeYears + 12;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+
+            if(ageInMonths == 9) {
+                humanAgeYears = humanAgeYears + 13;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+
+            if(ageInMonths == 10) {
+                humanAgeYears = humanAgeYears + 13;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+
+            if(ageInMonths == 11) {
+                humanAgeYears = humanAgeYears + 14;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+        } 
+
+        if(ageInMonths == 12) {
+            humanAgeYears = humanAgeYears + 15;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+
+        if(ageInMonths > 12 && ageInMonths < 18) {
+            humanAgeYears = ageInMonths + 3;
+            console.log(`humanAgeYears:`, humanAgeYears);
+            // 12          13          14          15          16          17          18
+            // 15          16          17          18          19          20          21
+        }
+
+        if(ageInMonths == 18) {
+            humanAgeYears = humanAgeYears + 21;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+
+        if(ageInMonths > 18 && ageInMonths < 24) {
+            if(ageInMonths == 19) {
+                humanAgeYears = humanAgeYears + 21;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+            if(ageInMonths == 20) {
+                humanAgeYears = humanAgeYears + 22;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+            if(ageInMonths == 11) {
+                humanAgeYears = humanAgeYears + 22;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+            if(ageInMonths == 22) {
+                humanAgeYears = humanAgeYears + 23;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+            if(ageInMonths == 23) {
+                humanAgeYears = humanAgeYears + 23;
+                console.log(`humanAgeYears:`, humanAgeYears);
+            }
+            // 18          19          20          21          22          23          24
+            // 21                      22                      23                      24
+        }
+
+        if(ageInMonths == 24) {
+            humanAgeYears = humanAgeYears + 24;
+            console.log(`humanAgeYears:`, humanAgeYears);
+        }
+    }
 
 
-const calculateAgeBtn = document.getElementById("calculate-age").value;
+    if(ageInMonths > 24) {
 
-calculateAgeBtn.addEventListener('click', () => {
-    calculateAgeFunction();
+        // 2 кал години    = 24 кал месеца;
+        // 24 чов години   = 288 чов месеца;
+
+        // 7 кал години    = 44 чов години;
+        // 84 кал месеца   = 528 чов месеца;
+
+        // 1 кал месец     = 4 чов месеца;
+
+
+        // 84 - 24 = 60 (кал месеци, без първите две години)
+        // 60 * 4 = 240 (кал месеци, превърнати в чов месеци)
+        // 240 + 288 = 528 (чов месеци, плюс първите две години в чов месеци)
+        // 528 / 12 = 44 (чов години)
+
+        var ageInMonthsWithoutTwoYears = (ageInMonths - 24);
+        var ageInMonthsWithoutTwoYearsHuman = ageInMonthsWithoutTwoYears * 4;
+        var ageInMontsHumanMonths = ageInMonthsWithoutTwoYearsHuman + 288;
+        console.log('Human Months', ageInMontsHumanMonths);
+        var ageInHumanYears = parseInt(ageInMontsHumanMonths / 12);
+        console.log(`Human Years`, ageInHumanYears);
+
+    }
 
 });
