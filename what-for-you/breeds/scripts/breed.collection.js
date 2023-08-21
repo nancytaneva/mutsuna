8f0581ba6db14d3acd6c341fcf0368f41a4f157c
Nancy Taneva <nancy.taneva@gmail.com>
2023-08-21 23:52:33 +0300

    Created Ready for a pet Page

--- what-for-you/breeds/scripts/breed.collection.js
+++ what-for-you/breeds/scripts/breed.collection.js
@@ -53,70 +53,281 @@ const breedCollection = [
         ownerExperience : 3
     }
 },
+
 {
-    id      : 'пудел',
-    name    : "Пудел",
-    nameEn  : "Пудел",
-    code    : "1152",
+    id      : 'Белгийска овчарка - Грюнендал',
+    name    : "Белгийска овчарка - Грюнендал",
+    nameEn  : "Belgian Shepherd Dog",
+    code    : "1232-1",
     general : {
-    country     : "Франция",
-    lifeLength  : "9-10 г."
+    country     : "Белгия",
+    lifeLength  : "0"
     },
     physicalTraits: {
-    height    : {
-        female    : "50-60 см",
-        male      : "10-65 см"
+    height      : {
+        female    : "58 см",
+        male      : "62 см"
     },
     weight      : {
-        female    : "22-32 кг",
-        male      : "300-400 кг"
+        female    : "20-25 кг",
+        male      : "25-30 кг"
     },
     },
-    history       : "Френския пудел е кучето на хомосексуалистите.",
+    history       : "Добави историята тук.",
     health        : "Добави имената на наследствените заболявания тук.",
     
     score: {
     family : {
-        affectionateWithFamily  : 1,
-        goodWithYoungChildren   : 1,
-        goodWithOtherDogs       : 1
+        affectionateWithFamily  : 3,
+        goodWithYoungChildren   : 3,
+        goodWithOtherDogs       : 3
+    },
+    physical: {
+        shedding                : 3,
+        grooming                : 2,
+        drooling                : 1,
+        coatLength              : 2
+    },
+    social : {
+        opennesToStrgangers     : 3,
+        playfulness             : 3,
+        protectiveNature        : 4,
+        adaptability            : 3
+    },
+    character : {
+        trainability            : 5,
+        energy                  : 4,
+        barking                 : 3,
+        mentalStimulationNeeds  : 4
+    }
+    },
+    selector: {
+        size            : "medium",
+        coatLengthWord  : "medium",
+        walkTime        : 3,
+        ownerExperience : 3
+    }
+},
+
+{
+    id      : 'Белгийска овчарка - Лакеноа',
+    name    : "Белгийска овчарка - Лакеноа",
+    nameEn  : "Belgian Shepherd Dog",
+    code    : "1232-2",
+    general : {
+    country     : "Белгия",
+    lifeLength  : "0"
+    },
+    physicalTraits: {
+    height      : {
+        female    : "58 см",
+        male      : "62 см"
+    },
+    weight      : {
+        female    : "20-25 кг",
+        male      : "25-30 кг"
+    },
+    },
+    history       : "Добави историята тук.",
+    health        : "Добави имената на наследствените заболявания тук.",
+    
+    score: {
+    family : {
+        affectionateWithFamily  : 3,
+        goodWithYoungChildren   : 3,
+        goodWithOtherDogs       : 3
+    },
+    physical: {
+        shedding                : 3,
+        grooming                : 2,
+        drooling                : 1,
+        coatLength              : 2
+    },
+    social : {
+        opennesToStrgangers     : 3,
+        playfulness             : 3,
+        protectiveNature        : 4,
+        adaptability            : 3
+    },
+    character : {
+        trainability            : 5,
+        energy                  : 4,
+        barking                 : 3,
+        mentalStimulationNeeds  : 4
+    }
+    },
+    selector: {
+        size            : "medium",
+        coatLengthWord  : "medium",
+        walkTime        : 3,
+        ownerExperience : 3
+    }
+},
+
+{
+    id      : 'Белгийска овчарка - Малиноа',
+    name    : "Белгийска овчарка - Малиноа",
+    nameEn  : "Belgian Shepherd Dog",
+    code    : "1232-3",
+    general : {
+    country     : "Белгия",
+    lifeLength  : "0"
+    },
+    physicalTraits: {
+    height      : {
+        female    : "58 см",
+        male      : "62 см"
+    },
+    weight      : {
+        female    : "20-25 кг",
+        male      : "25-30 кг"
+    },
+    },
+    history       : "Добави историята тук.",
+    health        : "Добави имената на наследствените заболявания тук.",
+    
+    score: {
+    family : {
+        affectionateWithFamily  : 3,
+        goodWithYoungChildren   : 3,
+        goodWithOtherDogs       : 3
+    },
+    physical: {
+        shedding                : 3,
+        grooming                : 2,
+        drooling                : 1,
+        coatLength              : 2
+    },
+    social : {
+        opennesToStrgangers     : 3,
+        playfulness             : 3,
+        protectiveNature        : 4,
+        adaptability            : 3
+    },
+    character : {
+        trainability            : 5,
+        energy                  : 4,
+        barking                 : 3,
+        mentalStimulationNeeds  : 4
+    }
+    },
+    selector: {
+        size            : "medium",
+        coatLengthWord  : "short",
+        walkTime        : 3,
+        ownerExperience : 3
+    }
+},
+
+{
+    id      : 'Белгийска овчарка - Тервюрен',
+    name    : "Белгийска овчарка - Тервюрен",
+    nameEn  : "Belgian Shepherd Dog",
+    code    : "1232-4",
+    general : {
+    country     : "Белгия",
+    lifeLength  : "0"
+    },
+    physicalTraits: {
+    height      : {
+        female    : "58 см",
+        male      : "62 см"
+    },
+    weight      : {
+        female    : "20-25 кг",
+        male      : "25-30 кг"
+    },
+    },
+    history       : "Добави историята тук.",
+    health        : "Добави имената на наследствените заболявания тук.",
+    
+    score: {
+    family : {
+        affectionateWithFamily  : 3,
+        goodWithYoungChildren   : 3,
+        goodWithOtherDogs       : 3
+    },
+    physical: {
+        shedding                : 3,
+        grooming                : 2,
+        drooling                : 1,
+        coatLength              : 2
+    },
+    social : {
+        opennesToStrgangers     : 3,
+        playfulness             : 3,
+        protectiveNature        : 4,
+        adaptability            : 3
+    },
+    character : {
+        trainability            : 5,
+        energy                  : 4,
+        barking                 : 3,
+        mentalStimulationNeeds  : 4
+    }
+    },
+    selector: {
+        size            : "medium",
+        coatLengthWord  : "medium",
+        walkTime        : 3,
+        ownerExperience : 3
+    }
+},
+
+{
+    id      : 'Бриар',
+    name    : "Бриар",
+    nameEn  : "Briard",
+    code    : "1142",
+    general : {
+    country     : "Франция",
+    lifeLength  : "12 г."
+    },
+    physicalTraits: {
+    height      : {
+        female    : "56-64 см",
+        male      : "62-68 см"
+    },
+    weight      : {
+        female    : "25-45 кг",
+        male      : "25-45 кг"
+    },
+    },
+    history       : "Добави историята тук.",
+    health        : "Добави имената на наследствените заболявания тук.",
+    
+    score: {
+    family : {
+        affectionateWithFamily  : 3,
+        goodWithYoungChildren   : 3,
+        goodWithOtherDogs       : 3
     },
     physical: {
         shedding                : 1,
-        grooming                : 1,
-        drooling                : 1,
-        coatLength              : 1,
+        grooming                : 4,
+        drooling                : 2,
+        coatLength              : 3
     },
     social : {
-        opennesToStrgangers     : 1,
-        playfulness             : 1,
-        protectiveNature        : 1,
-        adaptability            : 1
+        opennesToStrgangers     : 3,
+        playfulness             : 3,
+        protectiveNature        : 4,
+        adaptability            : 3
     },
     character : {
-        trainability            : 1,
-        energy                  : 1,
-        barking                 : 1,
-        mentalStimulationNeeds  : 1
+        trainability            : 5,
+        energy                  : 4,
+        barking                 : 3,
+        mentalStimulationNeeds  : 4
     }
-    }    
-},
-{
-    id      : 'френски пудел',
-    name    : "Френски Пудел",
-    nameEn  : "Пудел",
-    code    : "1162",
-},
-{
-    id      : 'български пудел',
-    name    : "Български Пудел",
-    nameEn  : "Български Пудел",
-    code    : "1153",
-},
-{
-    id      : 'дакел',
-    name    : "Дакел",
-    nameEn  : "Дакел",
-    code    : "1154",
+    },
+    selector: {
+        size            : "medium",
+        coatLengthWord  : "long",
+        walkTime        : 3,
+        ownerExperience : 3
+    }
 }
+
+
 ];
