b0695ea14a1856c735e4660cd0f38ed58208df23
Nancy Taneva <nancy.taneva@gmail.com>
2025-01-07 18:04:22 +0200

    Cat breed library searchbar working

--- what-for-you/breeds/scripts/breed.cat.library.js
+++ what-for-you/breeds/scripts/breed.cat.library.js
@@ -1,3 +1,29 @@
+//Function to scroll to the breed
+function scrollToBreed() {
+  // Scroll the breed-wrapper into view
+  const breedWrapper = document.getElementById("breed-wrapper");
+
+  // Calculate the offset for scrolling to the target section
+  const offset = -headerHeight;
+
+  // Scroll to the target section, applying the offset
+  window.scroll({
+    top: breedWrapper.getBoundingClientRect().top + window.scrollY + offset,
+    behavior: 'smooth'
+  });
+}
+
+// Function to handle card selection
+function onSelectCard(codeReference) {
+document.getElementById("breed-wrapper").style.display="block";
+
+const objectReference = breedCatCollection.find((element) => element.code == codeReference.code);
+
+$scoreCatComponent.render(objectReference);
+
+scrollToBreed()
+}
+
 // Render data to HTML
 const grid = document.getElementById('breedCatCollection-grid');
 
@@ -8,11 +34,9 @@ breedCatCollection.forEach((element) => {
   card.classList.add('breed-library-box');
   card.style.backgroundColor = 'white'; // Default to white background
   card.addEventListener('click', () => onSelectCard(element));
-  console.log("START")
 
     // Set the background image dynamically
     const imagePath = `./img/cat/${element.code}/1.png`;
-    console.log(`./img/cat/${element.code}/1.png`)
     const img = new Image();
     img.src = imagePath;
     
@@ -26,8 +50,6 @@ breedCatCollection.forEach((element) => {
       console.warn(`Image not found: ${imagePath}`);
       // Keep the white background
     };
-
-  console.log(`${element.name}`)
 
   // Add text content
   const content = `
---
+++ what-for-you/breeds/scripts/score.cat.component.js
@@ -0,0 +1,55 @@
+window.$scoreCatComponent = (function () {
+
+    function renderBreedInfo(breedInfo) {
+    
+        const breedInfoDiv     = document.getElementById("breedInfo");
+        breedInfoDiv.innerHTML = ""; // Clear previous content
+    
+        const breedNameDiv = document.createElement("div");
+        breedNameDiv.classList.add("card");
+        breedNameDiv.innerHTML = `
+                <p class="card-title">Порода:</p>
+                <p class="card-text">${breedInfo.name}</p>
+            `; 
+    
+        // const lifeLengthDiv = document.createElement("div");
+        // lifeLengthDiv.classList.add("card");
+        // lifeLengthDiv.innerHTML = `
+        //         <p class="card-title">Живот:</p>
+        //         <p class="card-text">${breedInfo.general.life_length_min}-${breedInfo.general.life_length_max} г.</p>
+        //     </div>
+        // `;
+    
+        const countryDiv = document.createElement("div");
+        countryDiv.classList.add("card");
+        countryDiv.innerHTML = `
+                <p class="card-title">Произход:</p>
+                <p class="card-text">${breedInfo.country}</p>
+            </div>
+        `;
+    
+        breedInfoDiv.appendChild(breedNameDiv);
+        // breedInfoDiv.appendChild(lifeLengthDiv);
+        breedInfoDiv.appendChild(countryDiv);
+
+        document.getElementById('breed-heading').innerHTML = `
+        <div class="heading">${breedInfo.name}</div>
+        <div class="choose-pet-text">(${breedInfo.nameEn})</div>
+        `;
+
+        document.getElementById('swiper-wrapper').innerHTML = `
+        <div class=""><img src='./img/cat/${breedInfo.code}/1.png' class="breed-img"></div>
+        `;
+
+        document.getElementById('breedInfoParagraph').innerHTML = `
+        <div class="contain p20">${breedInfo.intro}</div>`
+    }
+
+    function render(breedObject) {
+
+        renderBreedInfo(breedObject);
+    }
+
+    return { render }
+
+})();
